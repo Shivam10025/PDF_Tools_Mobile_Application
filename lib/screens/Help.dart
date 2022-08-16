19d8004754d00cdd950f38b11d5621f37ad89f77
Deepakgarg2309 <deepakgarg2309@gmail.com>
2022-08-16 14:16:18 +0530

    first commit

---
+++ lib/screens/Help.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class Help_info extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+
+  }
+}
+
+class Doc_scanner extends State<Doc_Scanner>{
+
+}
+
